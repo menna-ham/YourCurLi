23d3eb8662b5168ef96574e98329a083d8cde395
menna-ham <menna.hamouda58@gmail.com>
2023-02-14 11:48:33 +0200

    'submit'

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function show(elementID) {
+    // find the requested page and alert if it's not found
+    const ele = document.getElementById(elementID);
+    if (!ele) {
+      alert("no such element");
+      return;
+    }
+  
+    // get all pages, loop through them and hide them
+    const pages = document.getElementsByClassName('page');
+    for (let i = 0; i < pages.length; i++) {
+      pages[i].style.display = 'none';
+    }
+  
+    // then show the requested page
+    ele.style.display = 'block';
+}
